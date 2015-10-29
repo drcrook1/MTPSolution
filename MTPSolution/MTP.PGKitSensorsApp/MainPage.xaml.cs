708af455a5d7f55f33b913fb30c2c4caf3e83e05
David Crook <drcrook@outlook.com>
2015-10-29 11:50:14 -0400

    Lots of comments added

--- MTPSolution/MTP.PGKitSensorsApp/MainPage.xaml.cs
+++ MTPSolution/MTP.PGKitSensorsApp/MainPage.xaml.cs
@@ -1,23 +1,14 @@
 ﻿using Microsoft.IoT.DeviceCore.Adc;
-using Microsoft.IoT.Devices.Adc;
 using Microsoft.IoT.Devices.Sensors;
 using MTP.IoT.Devices.Adc;
 using System;
 using System.Collections.Generic;
-using System.IO;
 using System.Linq;
-using System.Runtime.InteropServices.WindowsRuntime;
 using Windows.Devices.Adc.Provider;
 using Windows.Devices.Gpio;
-using Windows.Foundation;
-using Windows.Foundation.Collections;
 using Windows.UI.Xaml;
 using Windows.UI.Xaml.Controls;
-using Windows.UI.Xaml.Controls.Primitives;
-using Windows.UI.Xaml.Data;
-using Windows.UI.Xaml.Input;
 using Windows.UI.Xaml.Media;
-using Windows.UI.Xaml.Navigation;
 using Microsoft.IoT.DeviceCore.Sensors;
 using Windows.UI;
 
@@ -30,50 +21,79 @@ namespace MTP.PGKitSensorsApp
     /// </summary>
     public sealed partial class MainPage : Page
     {
+        //Variables required for project
         private GpioController gpioController;
         private AdcProviderManager adcManager;
+        //List used to average values, as you will get weird sensory input occasionally.
+        //This helps smooth the output.
         private List<double> lightVals;
 
         public MainPage()
         {
             this.InitializeComponent();
+            //When page is loaded, we will do things as we do use the UI in this project.
             this.Loaded += MainPageLoaded;
         }
 
         private async void MainPageLoaded(object sender, RoutedEventArgs e)
         {
+            //Initialize objects
             gpioController = GpioController.GetDefault();
             IAdcControllerProvider MCP3008_SPI0 = new McpClassAdc();
             adcManager = new AdcProviderManager();
             lightVals = new List<double>();
+            //Insert 10 dummy values to initialize.
             for (int i = 0; i < 10; i++)
             {
                 lightVals.Add(0);
             }
+            //Initialize MCP3008 device.
+            //Remember, 8 channels, 10 bit resolution
+            //We wired up to SPI0 of pi with Chip select 0.
             ((McpClassAdc)MCP3008_SPI0).ChannelCount = 8;
             ((McpClassAdc)MCP3008_SPI0).ChannelMode = ProviderAdcChannelMode.SingleEnded;
             ((McpClassAdc)MCP3008_SPI0).ChipSelectLine = 0;
             ((McpClassAdc)MCP3008_SPI0).ControllerName = "SPI0";
             ((McpClassAdc)MCP3008_SPI0).ResolutionInBits = 10;
+            #region ADC Provider Stuff
+            //Add ADC Provider to list of providers
             adcManager.Providers.Add(MCP3008_SPI0);
+            //Get all ADC Controllers.
             var adcControllers = await adcManager.GetControllersAsync();
-
+            //This is just how its done. 
+            #endregion ADC Provider Stuff
             var lightSensor = new AnalogSensor()
             {
+                //Notice access via controller index.
+                //You will need to keep tabs on your 
+                //ADC providers locations in the list
+                //Channel 7 as this is where we wired the photo resistor to.
                 AdcChannel = adcControllers[0].OpenChannel(7),
+                //every 500 milliseconds, grab read the value.
                 ReportInterval = 500,
             };
+            //Attach a function to the event we fire every 500 milliseconds.
             lightSensor.ReadingChanged += LightSensor_ReadingChanged;
         }
-
+        /// <summary>
+        /// Event Handler for Light Sensor Reading Changed.
+        /// </summary>
+        /// <param name="sender"></param>
+        /// <param name="args"></param>
         private async void LightSensor_ReadingChanged(IAnalogSensor sender, AnalogSensorReadingChangedEventArgs args)
         {
-            // Invert
+            //Read the ratio, which is readvalue/maxvalue
             var reading = args.Reading.Ratio;
+            //Add reading to end of list
             lightVals.Add(reading);
+            //remove first reading.  Constant size of list is 10.
             lightVals.RemoveAt(0);
+            //Smooth the reading out by averaging all 10
+            //this is average light value across 5 seconds
+            //Note: You occasionally get bad values, hence the smoothing. 
             var avg = lightVals.Average();
             // Update UI
+            #region comment this out if running headless.
             await Dispatcher.RunIdleAsync((s) =>
             {
                 // Value
@@ -93,7 +113,7 @@ namespace MTP.PGKitSensorsApp
                     LightProgress.Foreground = new SolidColorBrush(Colors.Green);
                 }
             });
-
+            #endregion comment this out if running headless.
         }
     }
 }
