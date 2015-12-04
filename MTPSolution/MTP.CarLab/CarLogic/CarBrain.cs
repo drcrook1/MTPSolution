a2f0d540b6e5f68dcec634efc9fb013b37969814
David Crook <drcrook@outlook.com>
2015-12-04 17:48:09 -0500

    Updated Labs and Car Lab code.

--- MTPSolution/MTP.CarLab/CarLogic/CarBrain.cs
+++ MTPSolution/MTP.CarLab/CarLogic/CarBrain.cs
@@ -8,8 +8,15 @@ using System.Threading.Tasks;
 
 namespace MTP.CarLab.CarLogic
 {
+    /// <summary>
+    /// This is a class.  A class can be turned into an object of this type.
+    /// In this example, we can create an object of the type "CarBrain".
+    /// The program creates a car brain.
+    /// When a car brain is created it starts driving.
+    /// </summary>
     public class CarBrain
     {
+        //possible states the car can be in.
         public enum CarState
         {
             Unknown,
@@ -19,10 +26,15 @@ namespace MTP.CarLab.CarLogic
             TurningRight,
             TurningLeft
         }
+        //representation of the current state it is in.
         public CarState CurrentState;
+        //representation of motorA (this could be left or right)
         public GpioMotorController motorA;
+        //representation of motorB (this could be left or right)
         public GpioMotorController motorB;
         //MotorA Pin Definitions
+        //switching these will cause the motor to spin the opposite direction
+        //when calling forward or back.
         private const int motorAForwardPin = 24;
         private const int motorABackPin = 23;
         //MotorB Pin Definitions
@@ -31,42 +43,58 @@ namespace MTP.CarLab.CarLogic
         //Distance Sensor Pin Definitions
         private const int triggerPin = 22;
         private const int echoPin = 27;
-
+        //representation of the distance sensor.
         private HCSR04 distanceSensor;
         /// <summary>
         /// Initializes the Car Brain
+        /// This code runs when a new brain is created.
         /// </summary>
         public CarBrain()
         {
+            //sets the first state of the car to unknown.
             CurrentState = CarState.Unknown;
             //initialize our motors.
-            motorA = new GpioMotorController(motorAForwardPin, motorABackPin);
-            motorB = new GpioMotorController(motorBForwardPin, motorBBackPin);
-            distanceSensor = new HCSR04(triggerPin, echoPin);
-            //initialize first state.
-            //this.InitializeFirstState();
-            while(true)
+            this.Initialize();
+                        
+            while(true) //do stuff forever.
             {
+                //drive, this is where main logic happens.
                 this.Drive();
             }
         }
 
+        private void Initialize()
+        {
+            //initialize the motors.
+            motorA = new GpioMotorController(motorAForwardPin, motorABackPin);
+            motorB = new GpioMotorController(motorBForwardPin, motorBBackPin);
+            //initialize our senses.
+            distanceSensor = new HCSR04(triggerPin, echoPin);
+            this.Stop();
+            this.CurrentState = CarState.Stopped;
+        }
+        //This is the main logic for the brain.  This is where the thinking and acting and control happens.
         private void Drive()
         {
-            var dist = distanceSensor.GetDistance();
-            if (dist > 50 && this.CurrentState != CarState.DrivingForward)
-            {
-                this.DriveForward();
-            }
-            else if (dist < 50 && this.CurrentState == CarState.DrivingForward)
-            {
-                this.Stop();
-            }
-            else
-            {
-                this.TurnLeft();
-            }
-            
+            motorA.DriveForwardFull();
+
+            ////get distance with the sensor for how far we are away from things.
+            //var dist = distanceSensor.GetDistance();
+            ////if we are more than 50 cm and not currently driving forward, lets drive forward.
+            //if (dist > 50 && this.CurrentState != CarState.DrivingForward)
+            //{
+            //    this.DriveForward();
+            //}            
+            ////otherwise, if we are close to something and driving forward, lets stop.
+            //else if (dist < 50 && this.CurrentState == CarState.DrivingForward)
+            //{
+            //    this.Stop();
+            //}//in all other instances, turn left.
+            //else
+            //{
+            //    this.TurnLeft();
+            //}
+
         }
 
         private void DriveForward()
@@ -91,7 +119,7 @@ namespace MTP.CarLab.CarLogic
             motorB.StopForward();
             motorA.DriveForwardFull();
             this.CurrentState = CarState.TurningRight;
-            System.Threading.SpinWait.SpinUntil(() => { return false; }, 50);
+            System.Threading.SpinWait.SpinUntil(() => { return false; }, 100);
             motorA.StopForward();
             this.CurrentState = CarState.Stopped;
         }
@@ -102,7 +130,7 @@ namespace MTP.CarLab.CarLogic
             motorB.StopForward();
             motorB.DriveForwardFull();
             this.CurrentState = CarState.TurningLeft;
-            System.Threading.SpinWait.SpinUntil(() => { return false; }, 50);
+            System.Threading.SpinWait.SpinUntil(() => { return false; }, 100);
             motorB.StopForward();
             this.CurrentState = CarState.Stopped;
         }
